ea496a58e6242a6a87a38918066709ef8238590c
rupatul1995 <rupalichavan578@gmail.com>
2024-07-11 16:01:56 +0530

    update

--- day-0/src/App.js
+++ day-0/src/App.js
@@ -1,11 +1,12 @@
-
+import {Routes,Route} from "react-router-dom";
 import './App.css';
+import PumaHome from "./PumaHome";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route/>
+        <Route  path="/Home" element={<PumaHome/>}/>
       </Routes>
     </div>
   );
---
+++ day-0/src/PumaHome.jsx
@@ -0,0 +1,56 @@
+import "./home.css";
+
+
+function PumaHome(){
+    return(
+        <div>
+            <div class="miandiv">
+        <div class="extra">
+            <p>EXTRA 5% DISCOUNT FOR ALL ONLINE PAYMENTS</p>
+        </div>
+
+        <div class="nav">
+            <div class="nav1">
+                <img  src="https://assets.turbologo.com/blog/en/2019/11/19084917/puma-logo-cover-958x575.png" height="70px" />
+                </div>
+          
+                <div  class="nav2">
+                    <a>New</a>
+                    <a>Wonem</a>
+                    <a>Men</a>
+                    <a>Kids</a>
+                    <a>FentyxPuma</a>
+                    <a>Motorsport</a>
+                    <a>Collabration</a>
+                    <a>Sports</a>
+                    <a>Outlet</a>
+                </div>
+                
+                
+                <div class="nav3">
+                    <i class="fa-solid fa-magnifying-glass" ></i>
+                    <input class="input" placeholder="search" />
+
+                </div>
+
+
+                <div class="nav4">
+
+                    <i class="fa-regular fa-heart"></i>
+                    <i class="fa-solid fa-cart-shopping"></i>
+                    <i class="fa-regular fa-user"></i>
+                </div>
+                 
+          
+                </div>
+
+                <div class="lastbody"> 
+                    <p>EXTRA 15% OFF*</p>
+                    <p> *ON CLOTHING ABOVE ₹1999 | *ON FOOTWEAR ABOVE ₹3999</p>
+                    <p>Auto-appiled at checkout | "Exclusions apply</p>
+                </div>
+    </div>
+        </div>
+    );
+}
+export default PumaHome;
