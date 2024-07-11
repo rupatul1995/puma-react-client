0ba0be1892664782900ebe87f896f5b989d127c8
rupatul1995 <rupalichavan578@gmail.com>
2024-07-11 17:45:55 +0530

    update

--- day-0/src/PumaHome.jsx
+++ day-0/src/PumaHome.jsx
@@ -5,13 +5,13 @@ function PumaHome(){
     return(
         <div>
             <div class="miandiv">
-        <div class="extra">
-            <p>EXTRA 5% DISCOUNT FOR ALL ONLINE PAYMENTS</p>
-        </div>
+               <div class="extra">
+              <p>EXTRA 5% DISCOUNT FOR ALL ONLINE PAYMENTS</p>
+                </div>
 
-        <div class="nav">
-            <div class="nav1">
-                <img  src="https://assets.turbologo.com/blog/en/2019/11/19084917/puma-logo-cover-958x575.png" height="70px" />
+                <div class="nav">
+                   <div class="nav1">
+                <img  src="https://assets.turbologo.com/blog/en/2019/11/19084917/puma-logo-cover-958x575.png"  />
                 </div>
           
                 <div  class="nav2">
@@ -44,10 +44,21 @@ function PumaHome(){
           
                 </div>
 
-                <div class="lastbody"> 
+                {/* <div class="lastbody"> 
                     <p>EXTRA 15% OFF*</p>
                     <p> *ON CLOTHING ABOVE ₹1999 | *ON FOOTWEAR ABOVE ₹3999</p>
                     <p>Auto-appiled at checkout | "Exclusions apply</p>
+                </div> */}
+
+                <div className="divflat">
+                  <div className="seasonsale">
+                    <div className="seasonsale1" >
+                        <h1>END OF SEASON SALE</h1>
+                    </div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                  </div>
                 </div>
     </div>
         </div>
