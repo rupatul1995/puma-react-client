4e2a8fdc6385ea843ca422ea72c9ea1e606ee3c2
rupatul1995 <rupalichavan578@gmail.com>
2024-07-15 16:29:11 +0530

    update

--- day-0/src/App.js
+++ day-0/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import PumaHome from "./PumaHome";
 import Login from "./Login";
 import Register from "./Register";
+import MensTshirt from "./MensTshirt";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route  path="/" element={<PumaHome/>}/>
         <Route path="/page-login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/menstshirt" element={<MensTshirt/>}/>
       </Routes>
     </div>
   );
